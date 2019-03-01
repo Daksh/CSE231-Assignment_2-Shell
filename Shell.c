3f84214568237c1271ff92739648b8da80c317e0
Daksh Shah <daksh17336@iiitd.ac.in>
2019-03-01 14:59:10 +0530

    Finally works. Removed Memory Leaks and got a better version

--- Shell.c
+++ Shell.c
@@ -4,58 +4,47 @@
 #include <string.h> //for strlen(), strcmp()
 #include <stdlib.h> //for malloc()
 #include <stdbool.h> //for bool datatype
-// #include <ctype.h>
+#include <ctype.h> //isspace()
+#include <assert.h> 
 
 #define PROMPT "$ "
+#define ASSERTF true
 
-char* strip(char* str){
-	int len = strlen(str);
-	printf("strip: string Length %d\n", len);
+void strip(char* str){
+	int len = strlen(str); //strlen gives length of string, without '\0'
 
 	//Setting startI and endI to the first and last non-empty characters
 	int startI = 0;
 	int endI = len-1;
-	for(; str[startI]==' ' || str[startI]=='\t'; startI++);
-	for(; str[endI]==' ' || str[endI]=='\n' || str[endI]=='\t'; endI--);//maybe use isspace? from ctype.h
-	printf("strip: ASCII of last character: %d\n", str[endI]);
-
-	int newStrLen = endI-startI+1;
+	for(; startI < len && isspace(str[startI]); startI++);
+	for(; endI >= 0 && isspace(str[endI]); endI--);
 	
-	//If there is nothing but spaces in the str, then we return empty string
-	if(newStrLen<=0){
-		char* newStr = (char*)malloc(sizeof(char));
-		newStr[0] = '\0';
-		return newStr;
+	if(endI==-1){
+		str[0] = '\0';
+		return;
 	}
 
-	char* newStr = (char*)malloc(sizeof(char)*newStrLen);//[endI-startI+1];
+	if(ASSERTF) assert(str[len]=='\0');
+	if(ASSERTF) assert(endI<len && endI>=0);
+	if(ASSERTF) assert(startI<len && startI>=0);
+	if(ASSERTF) assert(startI<=endI);
 
-	for(int i=startI; i<=endI; i++)
-		newStr[i-startI] = str[i];
-	printf("strip: startI %d   endI %d\n", startI, endI);
-	printf("strip: returning %s%s%s\n", "XXX",newStr,"XXX");
-	return newStr;
+	int i;
+	for(i = startI; i<=endI; i++)
+		str[i-startI] = str[i];
+	str[i-startI] = '\0';
 }
 
 int main (){
 	char* command;
-	bool exitFlag = false;
-	printf("Space: %d\n", ' ');
-	printf("Newline: %d\n", '\n');
-	printf("Tab: %d\n", '\t');
-	while(!exitFlag){
+
+	while(strcmp(command,"exit")!=0){
 		printf(PROMPT);
 
 		scanf("%[^\n]",command);
-		getchar();//to discard the newline character from STDIN
-		printf("%s%s%s\n", "XXX",command,"XXX");
-		printf("main: strLen %lu\n", strlen(command));
-		command = strip(command);
-		printf("%s%s%s\n", "XXX",command,"XXX");
-
-		exitFlag = strcmp(command,"exit")==0;
-		// printf("%d\n", exitFlag);
-		free(command);
+		getchar();//to discard the newline character from STDIN		
+		strip(command);
+		
 	}
 	// int pid, i, status;
 	// printf ("main %d parent %d\n", getpid(), getppid());
@@ -76,4 +65,5 @@ int main (){
 /*
 1. In case strip does not work properly, can refer to Linux's implementation at https://stackoverflow.com/a/1488419/2806163
 2. String slice: https://stackoverflow.com/a/26620524/2806163
+3. isspace - https://www.programiz.com/c-programming/library-function/ctype.h/isspace
 */
--- test.c
+++ test.c
@@ -5,10 +5,7 @@ int main(){
 	char* command;
 	scanf("%[^\n]",command);
 	getchar();//to discard the newline character from STDIN
-	printf("%s\n", command);
-	scanf("%[^\n]",command);
-	getchar();//to discard the newline character from STDIN
-	printf("%s\n", command);
-	printf("%d\n", strcmp(command, "daksh")); 
+	printf("strLen: %lu\n", strlen(command));
+	printf("%d\n", command[strlen(command)]);
 	return 0;
 }
