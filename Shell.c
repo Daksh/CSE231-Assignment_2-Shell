188d7ba122cda139b7708a55c8d5b4f1d0b8baa9
Daksh Shah <daksh17336@iiitd.ac.in>
2019-03-01 14:24:04 +0530

    Werent adding the null char at end of newStr

--- Shell.c
+++ Shell.c
@@ -4,18 +4,21 @@
 #include <string.h> //for strlen(), strcmp()
 #include <stdlib.h> //for malloc()
 #include <stdbool.h> //for bool datatype
+// #include <ctype.h>
 
 #define PROMPT "$ "
 
 char* strip(char* str){
 	int len = strlen(str);
+	printf("strip: string Length %d\n", len);
 
 	//Setting startI and endI to the first and last non-empty characters
 	int startI = 0;
 	int endI = len-1;
 	for(; str[startI]==' ' || str[startI]=='\t'; startI++);
-	for(; str[endI]==' ' || str[endI]=='\n' || str[endI]=='\t'; endI--);
-	
+	for(; str[endI]==' ' || str[endI]=='\n' || str[endI]=='\t'; endI--);//maybe use isspace? from ctype.h
+	printf("strip: ASCII of last character: %d\n", str[endI]);
+
 	int newStrLen = endI-startI+1;
 	
 	//If there is nothing but spaces in the str, then we return empty string
@@ -29,18 +32,24 @@ char* strip(char* str){
 
 	for(int i=startI; i<=endI; i++)
 		newStr[i-startI] = str[i];
+	printf("strip: startI %d   endI %d\n", startI, endI);
+	printf("strip: returning %s%s%s\n", "XXX",newStr,"XXX");
 	return newStr;
 }
 
 int main (){
 	char* command;
 	bool exitFlag = false;
+	printf("Space: %d\n", ' ');
+	printf("Newline: %d\n", '\n');
+	printf("Tab: %d\n", '\t');
 	while(!exitFlag){
 		printf(PROMPT);
 
 		scanf("%[^\n]",command);
 		getchar();//to discard the newline character from STDIN
 		printf("%s%s%s\n", "XXX",command,"XXX");
+		printf("main: strLen %lu\n", strlen(command));
 		command = strip(command);
 		printf("%s%s%s\n", "XXX",command,"XXX");
 
@@ -66,5 +75,5 @@ int main (){
 
 /*
 1. In case strip does not work properly, can refer to Linux's implementation at https://stackoverflow.com/a/1488419/2806163
-
+2. String slice: https://stackoverflow.com/a/26620524/2806163
 */
